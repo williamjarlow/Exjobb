991393497fb78bc9edb28b5ac33578ea535eb0ad
williamjarlow <38031435+williamjarlow@users.noreply.github.com>
2019-02-24 15:15:08 +0100

    Changed movement system

---
+++ Impetus/Assets/Scripts/Player/GroundedMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundedMovement : Movement {
+    Movement movement;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (onGround)
+            jumpCharges = maxJumpCharges;
+    }
+}
--- Impetus/Assets/Scripts/Player/Movement.cs
+++ Impetus/Assets/Scripts/Player/Movement.cs
@@ -1,61 +1,94 @@
 ﻿using UnityEngine;
 using System.Collections;
-using AnimatorStateMachineUtil;
 
-/*public enum ParamType
-{
-    TRIGGER,
-    BOOL,
-    INT,
-    FLOAT,
-    Count
-}
-
-[System.Serializable]
-public class Parameter
-{
-    public ParamType paramType;
-    public string name;
-}
-*/
 public class Movement : MonoBehaviour
 {
     [SerializeField]
-    string movingName, jumpName, jumpChargesName, onGroundName, maxJumpChargesName;
+    float acceleration, deceleration, maxSpeed, jumpSpeed;
+    [SerializeField]
     int maxJumpCharges, jumpCharges;
-
+    Rigidbody2D thisRB2D;
     Animator animator;
+    SpriteRenderer spriteRenderer;
+    bool onGround;
 
     // Use this for initialization
     void Start()
     {
+        thisRB2D = GetComponentInParent<Rigidbody2D>();
+        spriteRenderer = GetComponentInParent<SpriteRenderer>();
         animator = GetComponent<Animator>();
-        maxJumpCharges = animator.GetInteger(maxJumpChargesName);
     }
 
     void Update()
     {
-        animator.SetBool(movingName, Input.GetAxis("Horizontal") != 0);
 
-        if (animator.GetBool(onGroundName))
-            animator.SetInteger(jumpChargesName, maxJumpCharges);
+        if (onGround)
+            jumpCharges = maxJumpCharges;
 
-        if (Input.GetButtonDown("Jump") && animator.GetInteger(jumpChargesName) > 0)
+        MoveHorizontal();
+        if (Input.GetButtonDown("Jump") && jumpCharges > 0)
         {
-            animator.SetTrigger(jumpName);
-            animator.SetBool(onGroundName, false);
+            Jump();
         }
+    }
+
+    void Jump()
+    {
+        jumpCharges--;
+        thisRB2D.velocity = new Vector2(thisRB2D.velocity.x, jumpSpeed);
+    }
+
+    void MoveHorizontal()
+    {
+        float direction = Mathf.Sign(thisRB2D.velocity.x);
+        float inputDirection = Input.GetAxisRaw("Horizontal");
+        if (onGround) //Ground movement
+        {
+            if (inputDirection != 0)
+            {
+                if (Mathf.Abs(thisRB2D.velocity.x) > maxSpeed)
+                    thisRB2D.velocity -= new Vector2(deceleration * direction, 0);
+                else
+                    thisRB2D.velocity += new Vector2(acceleration * inputDirection, 0);
+                spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") > 0;
+            }
+            else
+            {
+                thisRB2D.velocity -= new Vector2(deceleration * direction, 0);
+                if (thisRB2D.velocity.x < 0 && direction > 0 ||
+                    thisRB2D.velocity.x > 0 && direction < 0)
+                    thisRB2D.velocity = new Vector2(0, thisRB2D.velocity.y);
+            }
+        }
+        else //Air movement
+        {
+            if (inputDirection != 0)
+            {
+                spriteRenderer.flipX = inputDirection > 0;
+                if (inputDirection < 0)
+                {
+                    if (thisRB2D.velocity.x > -maxSpeed)
+                        thisRB2D.velocity -= new Vector2(acceleration, 0);
+                }
+                else if (inputDirection > 0)
+                {
+                    if (thisRB2D.velocity.x < maxSpeed)
+                        thisRB2D.velocity += new Vector2(acceleration, 0);
+                }
+            }
+        }
+
     }
 
     void OnCollisionEnter2D(Collision2D other)
     {
-        GetComponent<Animator>().SetBool(onGroundName, true);
-        GetComponent<Animator>().SetTrigger("Change");
+        onGround = true;
     }
 
     void OnCollisionExit2D(Collision2D other)
     {
-        GetComponent<Animator>().SetBool(onGroundName, false);
-        GetComponent<Animator>().SetTrigger("Change");
+        onGround = false;
+        jumpCharges--;
     }
 }
