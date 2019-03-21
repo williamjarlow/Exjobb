4e036d35830b6426f636b75f81a461638d8e99b5
BSpong <william.jarlow@gmail.com>
2019-03-21 16:14:48 +0100

    Attack system WORKS!!!
    $w4gM0ney

---
+++ Impetus/Assets/AttackBehaviour.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TypeReferences;
+
+public class AttackBehaviour : MonoBehaviour
+{
+    [SerializeField] List<Sprite> spriteList;
+    [SerializeField] [ClassExtends(typeof(Skill))] ClassTypeReference skill;
+    SpriteRenderer spriteRenderer;
+    int index;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NextSprite()
+    {
+        spriteRenderer.sprite = spriteList[index++];
+    }
+
+    public void EndAnimation()
+    {
+        Destroy(gameObject);
+    }
+
+    public void EndAttack()
+    {
+        GetComponentInParent<Animator>().SetTrigger("Idle");
+        Skill _skill = (Skill)FindObjectOfType(skill.Type);
+        _skill.skillActive = false;
+    }
+
+}
