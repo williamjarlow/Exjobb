d44d69d1c831e8e705d824b1a61615ac89160314
BSpong <william.jarlow@gmail.com>
2019-03-29 17:56:10 +0100

    Trigger fixed, target particles done

--- Impetus/Assets/Trigger.cs
+++ Impetus/Assets/Trigger.cs
@@ -10,12 +10,12 @@ public class Trigger : MonoBehaviour
     bool permanent;
     [SerializeField]
     DoorHandler[] doors;
-    bool flag;
+    bool flag, triggered;
+    List<Collider2D> actors = new List<Collider2D>();
     [SerializeField]
     Sprite activated, deactivated;
-
-
     SpriteRenderer spriteRenderer;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -26,29 +26,61 @@ public class Trigger : MonoBehaviour
     void FixedUpdate()
     {
         flag = true;
+        if(actors.Count > 0)
+        {
+            bool exists = true;
+            foreach(Collider2D actor in actors)
+            {
+                if (actor == null)
+                    exists = false;
+            }
+            if (!exists)
+                Deactivate();
+        }
+
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == agentTag)
+        {
+            actors.Add(collision);
+            Activate();
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == agentTag) {
-            flag = false;
-            spriteRenderer.sprite = activated;
-
-            foreach(DoorHandler door in doors)
-            {
-                door.Open();
-            }
+        if (collision.gameObject.tag == agentTag)
+        {
+            Activate();
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == agentTag && !permanent && flag) {
-            spriteRenderer.sprite = deactivated;
-            foreach (DoorHandler door in doors)
-            {
-                door.Close();
-            }
+            Deactivate();
+        }
+    }
+
+    void Activate()
+    {
+        flag = false;
+        spriteRenderer.sprite = activated;
+
+        foreach (DoorHandler door in doors)
+        {
+            door.Open();
+        }
+    }
+
+    void Deactivate()
+    {
+        spriteRenderer.sprite = deactivated;
+        foreach (DoorHandler door in doors)
+        {
+            door.Close();
         }
     }
 }
