8a79c6d1ca34aa2e788e7864f6c3f88f5d3c88df
williamjarlow <38031435+williamjarlow@users.noreply.github.com>
2019-04-02 15:51:33 +0200

    Push with error

--- Impetus/Assets/Scripts/GameManager.cs
+++ Impetus/Assets/Scripts/GameManager.cs
@@ -11,11 +11,20 @@ public class GameManager : MonoBehaviour
     [SerializeField]
     int sceneIndex = 1;
 
+    GameObject victoryText;
+
+
     // Start is called before the first frame update
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
         instance = gameObject.GetComponent<GameManager>();
+    }
+
+    private void Start()
+    {
+        victoryText = GameObject.FindWithTag("VictoryText");
+        victoryText.SetActive(false);
     }
 
     // Update is called once per frame
@@ -28,14 +37,22 @@ public class GameManager : MonoBehaviour
 
     public void OnDummyDestroy()
     {
+        GameObject.FindWithTag("Player").GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+        FindObjectOfType<Movement>().enabled = false;
+        FindObjectOfType<ManageSkills>().enabled = false;
+        victoryText.SetActive(true);
         StartCoroutine("LoadNextScene");
     }
 
     IEnumerator LoadNextScene()
     {
-        yield return new WaitForSeconds(5);
-        Debug.Log("Loading");
+        yield return new WaitForSeconds(2);
         SceneManager.LoadScene(++sceneIndex);
     }
 
+    private void OnLevelWasLoaded(int level)
+    {
+        victoryText = GameObject.FindWithTag("VictoryText");
+        victoryText.SetActive(false);
+    }
 }
