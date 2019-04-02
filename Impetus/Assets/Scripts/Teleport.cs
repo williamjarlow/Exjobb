876800bb25a52c093a2109479e2f6aff2b01f508
williamjarlow <38031435+williamjarlow@users.noreply.github.com>
2019-04-02 16:52:28 +0200

    Teleport changed

---
+++ Impetus/Assets/Scripts/Teleport.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    [SerializeField]
+    GameObject agent, target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject == agent)
+            agent.transform.position = target.transform.position;
+    }
+}
