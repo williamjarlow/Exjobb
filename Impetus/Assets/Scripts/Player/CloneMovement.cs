9e55c621c4eddf146b8543f9da755363124afcf6
BSpong <william.jarlow@gmail.com>
2019-04-04 13:12:32 +0200

    Rensande, pilar wip

--- Impetus/Assets/Scripts/Player/CloneMovement.cs
+++ Impetus/Assets/Scripts/Player/CloneMovement.cs
@@ -3,30 +3,6 @@ using System.Collections;
 
 public class CloneMovement : MonoBehaviour
 {
-    [SerializeField]
-    [Header("Horizontal variables")]
-    [Tooltip("Max horizontal speed.")]
-    float maxSpeed = 5;
-
-    [SerializeField]
-    [Tooltip("Max horizontal speed while sprinting.")]
-    float sprintMaxSpeed = 5;
-
-    [SerializeField]
-    [Tooltip("Adds to your RB2D.velocity when moving.")]
-    float accelConst = 2.5f;
-
-    [SerializeField]
-    [Tooltip("Adds to your RB2D.velocity while sprinting.")]
-    float sprintAccelConst = 2.5f;
-
-    [SerializeField]
-    [Tooltip("Subtracts from your RB2D.velocity when not moving.")]
-    float frictionConst = 0.6f;
-
-    [SerializeField]
-    [Tooltip("How strong friction is in the air. 1 = 100% strength.")]
-    float airFrictionMult = 0.6f;
 
     [SerializeField]
     [Header("Vertical variables")]
@@ -37,43 +13,6 @@ public class CloneMovement : MonoBehaviour
     [Tooltip("Max falling speed.")]
     float terminalVelocity = 10;
 
-    [SerializeField]
-    [Tooltip("Added every frame of a jump, kind of.")]
-    int jumpHeight = 4;
-
-    [SerializeField]
-    [Tooltip("Buffer time when pressing jump before hitting a platform.")][Header ("Time variables in frames")]
-    int jumpBufferMax = 3;
-
-    [SerializeField]
-    [Tooltip("Grace time when falling off a platform.")]
-    int jumpGraceMax = 2;
-
-    [SerializeField]
-    [Tooltip("Max time holding space will make you jump for.")]
-    int jumpMaxTime = 15;
-
-    [SerializeField]
-    [Tooltip("Minimum jump time.")]
-    int jumpMinTime = 2;
-
-    [SerializeField]
-    [Tooltip("How long it takes for a jump to reach the apex.")]
-    int jumpChangeTime = 7;
-
-    [SerializeField]
-    [Tooltip("Divides upward RB2D.velocity by this when letting go of space early in a jump.")]
-    int jumpCutoffDiv = 7;
-
-    [SerializeField]
-    [Header("Jump charge variables")]
-    [Tooltip("How long it takes for the jump to charge.")]
-    int jumpChargeTime = 15;
-
-    [SerializeField]
-    [Tooltip("Height multiplier for charged jumps.")]
-    float jumpChargeMult = 1.2f;
-
     Rigidbody2D RB2D;
     Animator animator;
     SpriteRenderer spriteRenderer;
@@ -82,9 +21,6 @@ public class CloneMovement : MonoBehaviour
     [HideInInspector]
     public bool onGround;
 
-    bool canJump, jumpActive, jumpCharged, sprinting;
-    int jumpBuffer, jumpTimer, jumpGraceTimer, jumpChargeTimer;
-    float jumpTimePercentage, _jumpChargeMult = 1;
     Vector2 velocity;
 
     [SerializeField]
@@ -103,14 +39,7 @@ public class CloneMovement : MonoBehaviour
 
     void Update()
     {
-        MoveHorizontal();
         MoveVertical();
-    }
-
-    void MoveHorizontal()
-    {
-        if (Input.GetAxisRaw("Horizontal") < 0 || Input.GetAxisRaw("Horizontal") > 0)
-            spriteRenderer.flipX = Input.GetAxisRaw("Horizontal") > 0;
     }
 
     void MoveVertical()
@@ -126,7 +55,6 @@ public class CloneMovement : MonoBehaviour
         if (temp == environmentLayerMask.value)
         {
             onGround = true;
-            _jumpChargeMult = 1;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
---
+++ Impetus/Assets/Scripts/TargetArrow.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetArrow : MonoBehaviour
+{
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetRotation();
+        SetPosition();
+    }
+
+    void SetPosition()
+    {
+        
+    }
+
+    void SetRotation()
+    {
+        Vector3 dir = player.transform.position - transform.parent.position;
+        dir = player.transform.InverseTransformDirection(dir);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
